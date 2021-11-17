167bac53a7cda550a58c818ebe4d5a71c7390ba7
huudungmanabian <huudung.nguyen@manabie.com>
2021-11-17 10:34:43 +0700

    Create first app

---
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.note
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onAddNote(view: View) {
+        val intent = Intent(this, CreateNote::class.java);
+        startActivity(intent)
+    }
+}
